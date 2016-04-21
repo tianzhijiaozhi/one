5324dbba59ba1da0094b7dbb81336dd8f21dc1d3
tianzhijiaozhi <qidouhai@163.com>
2016-04-21 12:00:28 +0800

    Update job.js

--- job.js
+++ job.js
@@ -77,7 +77,7 @@ function addCloudLayer() {
             },
             clickable: true
         };
-        cloudDataLayer = new AMap.CloudDataLayer('165f535de21755f3d578f4adeab3be95', layerOptions); //实例化云图层类
+        cloudDataLayer = new AMap.CloudDataLayer('5718501e7bbf197be3251d2e', layerOptions); //实例化云图层类
         cloudDataLayer.setMap(map); //叠加云图层到地图
         
         AMap.event.addListener(cloudDataLayer, 'click', function(result) {
